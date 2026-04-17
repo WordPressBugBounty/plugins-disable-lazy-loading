ec471247d1766ba62fcdbb09516f8756ef7fc05b
Sean Murphy <sean@iamseanmurphy.com>
2026-04-17 21:26:30 +0000

    2.6

--- disable-lazy-loading/disable-lazy-loading.php
+++ disable-lazy-loading/disable-lazy-loading.php
@@ -9,9 +9,9 @@
 	Donate link: https://monzillamedia.com/donate.html
 	Contributors: specialk
 	Requires at least: 5.4
-	Tested up to: 6.9
-	Stable tag: 2.5
-	Version:    2.5
+	Tested up to: 7.0
+	Stable tag: 2.6
+	Version:    2.6
 	Requires PHP: 5.6.20
 	Text Domain: disable-lazy-loading
 	Domain Path: /languages
